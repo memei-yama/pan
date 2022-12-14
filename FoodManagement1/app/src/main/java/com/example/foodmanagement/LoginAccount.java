6190e95e6f94b7fb9e7a27736b83f36e1276d474
yamasaki-risa <risakinoko@icloud.com>
2022-12-14 09:22:10 +0000

    画面遷移ができるようになりました

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/LoginAccount.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/LoginAccount.java
@@ -1,2 +1,18 @@
-package com.example.foodmanagement;public class LoginAccount {
+//山﨑里沙,2022/12/14
+//ログインをするプログラム
+package com.example.foodmanagement;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoginAccount extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginaccount);
+
+        //Intent intent = getIntent();
+
+    }
 }
