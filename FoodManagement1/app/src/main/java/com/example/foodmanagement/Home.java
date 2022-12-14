a80e7fdbee658396d6e16508fedb2f67c2c4b382
memei-yama <mei01yam12@yahoo.co.jp>
2022-12-14 15:43:08 +0900

    ホーム画面

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/Home.java
@@ -0,0 +1,25 @@
+//山口芽依,2022/12/14
+//ホーム画面のプログラム
+
+package com.example.foodmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //ここをホームに変える
+
+        //ボタンの設定
+        //Button food_register = findViewById(R.id.food_register);
+        //Button food_search = findViewById(R.id.food_search);
+        //Button food_sort = findViewById(R.id.food_sort);
+        //Button favorite = findViewById(R.id.favorite);
+        //Button delete_mode = findViewById(R.id.delete_mode);
+
+    }
+}
