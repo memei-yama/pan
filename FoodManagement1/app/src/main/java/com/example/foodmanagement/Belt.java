81d2ab5bc02b635b0976b102e433287b65d748fe
yamasaki-risa <risakinoko@icloud.com>
2022-12-15 05:56:38 +0000

    ベルトモジュールのファイル

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/Belt.java
@@ -0,0 +1,15 @@
+//山﨑里沙,2022/12/15
+//下のアイコンを押したら遷移するプログラム
+package com.example.foodmanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Belt extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.belt);
+    }
+}
