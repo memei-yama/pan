88606a3fea98e7d9595a5e10f5dc6f4db36c241f
yamasaki-risa <risakinoko@icloud.com>
2022-12-15 05:59:12 +0000

    ユーザの初期画面のプログラム

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/Account.java
@@ -0,0 +1,13 @@
+package com.example.foodmanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Account extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_account);
+    }
+}
