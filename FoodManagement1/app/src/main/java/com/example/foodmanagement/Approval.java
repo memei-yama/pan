de103d2eab25d5083451492d93b50a297b3ba87b
yamasaki-risa <risakinoko@icloud.com>
2022-12-15 07:27:00 +0000

    承認を行うプログラム

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/Approval.java
@@ -0,0 +1,15 @@
+//山﨑里沙,2022/12/15
+//承認を行うプログラム
+package com.example.foodmanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Approval extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.approval);
+    }
+}
