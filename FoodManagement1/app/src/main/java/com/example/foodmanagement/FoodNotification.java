93f03c9266b4474873c08ab4d51947685530b98f
memei-yama <mei01yam12@yahoo.co.jp>
2022-12-18 18:17:48 +0900

    食品の期限を通知するプログラム

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/FoodNotification.java
@@ -0,0 +1,16 @@
+//山口芽依,2022/12/18
+//食品の期限を通知するプログラム
+
+package com.example.foodmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodNotification extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage);
+    }
+}
