e9685b6aaeaa606c5e3391b88bbbdc5a8a82c73d
memei-yama <mei01yam12@yahoo.co.jp>
2022-12-18 17:58:41 +0900

    ユーザ検索のプログラム

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/SearchUser.java
@@ -0,0 +1,16 @@
+//山口芽依,2022/12/18
+//ユーザ検索のプログラム
+
+package com.example.foodmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchUser extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage);
+    }
+}
