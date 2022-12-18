33242f34e3f9eb3f56ce178c57353786784cddbe
memei-yama <mei01yam12@yahoo.co.jp>
2022-12-18 18:02:03 +0900

    家族とフレンドの情報を取得するプログラム

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/GetFamilyFriendInfo.java
@@ -0,0 +1,16 @@
+//山口芽依,2022/12/18
+//家族・フレンドの情報を取得するのプログラム
+
+package com.example.foodmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetFamilyFriendInfo extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage);
+    }
+}
