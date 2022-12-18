01718d0a784abb06392fa7dfea9bf40ca2946141
memei-yama <mei01yam12@yahoo.co.jp>
2022-12-18 18:28:59 +0900

    食品のお気に入りを表示するプログラム

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/FavoriteView.java
@@ -0,0 +1,16 @@
+//山口芽依,2022/12/18
+//食品のお気に入りを表示するプログラム
+
+package com.example.foodmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavoriteView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage);
+    }
+}
