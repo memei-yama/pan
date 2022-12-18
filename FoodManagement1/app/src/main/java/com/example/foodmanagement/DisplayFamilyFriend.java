dcf136a56432a671d270542f4007d453920e5319
memei-yama <mei01yam12@yahoo.co.jp>
2022-12-18 18:04:35 +0900

    家族とフレンドを表示させるプログラム

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/DisplayFamilyFriend.java
@@ -0,0 +1,16 @@
+//山口芽依,2022/12/18
+//家族・フレンドを表示させるプログラム
+
+package com.example.foodmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayFamilyFriend extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mypage);
+    }
+}
