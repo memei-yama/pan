9ee8527e9325a760a79e6705123f52ba08140020
小川歩華 <ogawafuka@301MacBook-Pro.local>
2022-12-19 15:04:32 +0900

    タイムラインを表示するプログラム

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/timelineMain.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/timelineMain.java
@@ -1,2 +1,15 @@
-package com.example.foodmanagement;public class timelineMain {
+//小川歩華，2022/12/19
+//タイムライン画面を表示するプログラム
+package com.example.foodmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class timelineMain {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timeline_main);
+    }
 }
