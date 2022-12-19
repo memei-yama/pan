05c4c213bd9a2d0308d22d9aefbec861eb085483
小川歩華 <ogawafuka@301MacBook-Pro.local>
2022-12-19 15:05:55 +0900

    タイムラインにリアクションするプログラム

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/timelineReaction.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/timelineReaction.java
@@ -1,2 +1,15 @@
-package com.example.foodmanagement;public class timelineReaction {
+//小川歩華，2022/12/19
+//タイムラインにリアクションを追加するプログラム
+package com.example.foodmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class timelineReaction {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timeline_reaction);
+    }
 }
