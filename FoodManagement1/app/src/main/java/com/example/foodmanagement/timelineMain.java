fe95dc1633d911d8229cfb0f5e7a6f6757b1f163
小川歩華 <ogawafuka@kut192-168-10-026.jim.kochi-tech.ac.jp>
2022-12-19 18:28:38 +0900

    public class にextends AppCompatActivityを追加

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/timelineMain.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/timelineMain.java
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class timelineMain {
+public class timelineMain extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
