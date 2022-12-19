d8889a36797bd1a83682ce0e23c740a0168f8e20
小川歩華 <ogawafuka@301MacBook-Pro.local>
2022-12-19 15:02:05 +0900

    メッセージを受信するプログラム

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/TextRec.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/TextRec.java
@@ -1,2 +1,15 @@
-package com.example.foodmanagement;public class TextRec {
+//小川歩華，2022/12/19
+//メッセージを受信するプログラム
+package com.example.foodmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextRec {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_rec);
+    }
 }
