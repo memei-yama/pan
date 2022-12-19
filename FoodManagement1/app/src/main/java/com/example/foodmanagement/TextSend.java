55382726e1afce1b6b5b1161b4e4498d63945d6e
小川歩華 <ogawafuka@301MacBook-Pro.local>
2022-12-19 15:03:21 +0900

    メッセージを送信するプログラム

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/TextSend.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/TextSend.java
@@ -1,2 +1,15 @@
-package com.example.foodmanagement;public class TextSend {
+//小川歩華，2022/12/19
+//メッセージを送信するプログラム
+package com.example.foodmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TextSend {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_send);
+    }
 }
