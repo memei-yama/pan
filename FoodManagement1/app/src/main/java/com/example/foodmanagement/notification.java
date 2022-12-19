fca946b3b54ce778c94e3c30d50ec99c62078f23
小川歩華 <ogawafuka@301MacBook-Pro.local>
2022-12-19 15:13:36 +0900

    通知を処理するプログラム

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/notification.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/notification.java
@@ -1,2 +1,15 @@
-package com.example.foodmanagement;public class notification {
+//小川歩華，2022/12/19
+//通知処理を行うプログラム
+package com.example.foodmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class notification {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
 }
