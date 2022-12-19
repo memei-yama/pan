f038b47e27b257315fba67faf324412405452fb5
小川歩華 <ogawafuka@301MacBook-Pro.local>
2022-12-19 14:57:14 +0900

    トーク一覧を表示する画面

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/TalkMain.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/TalkMain.java
@@ -1,2 +1,15 @@
-package com.example.foodmanagement;public class TalkMain {
+//小川歩華，2022/12/19
+//トーク一覧を表示するプログラム
+package com.example.foodmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TalkMain {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.talk_main);
+    }
 }
