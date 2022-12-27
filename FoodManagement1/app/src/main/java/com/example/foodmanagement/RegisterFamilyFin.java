3217b2d29ab4cadcd276f108f6a9072f9d409337
yamasaki-risa <risakinoko@icloud.com>
2022-12-27 04:44:40 +0000

    家族登録完了を行うプログラム

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/RegisterFamilyFin.java
@@ -0,0 +1,15 @@
+//山﨑里沙,2022/12/27
+//家族登録の完了画面プログラム
+package com.example.foodmanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterFamilyFin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_familyfin);
+    }
+}
