9dba6047c51395077d452a211aafea51cdd8778a
yamasaki-risa <risakinoko@icloud.com>
2022-12-27 04:42:54 +0000

    家族ユーザの確認を行うプログラム

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/RegisterFamilyCheck.java
@@ -0,0 +1,15 @@
+//山﨑里沙,2022/12/27
+//追加する家族ユーザの確認プログラム
+package com.example.foodmanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterFamilyCheck extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_familycheck);
+    }
+}
---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/RegisterFamilySearch2.java
@@ -0,0 +1,15 @@
+//山﨑里沙,2022/12/27
+//家族のユーザ検索の結果を表示するプログラム
+package com.example.foodmanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterFamilySearch2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_family2);
+    }
+}
