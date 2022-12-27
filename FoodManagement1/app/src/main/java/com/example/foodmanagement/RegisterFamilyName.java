40d2de3366a0eadc6c5d566ff9afabb0f2027478
yamasaki-risa <risakinoko@icloud.com>
2022-12-27 04:35:38 +0000

    家族のグループ名を入力するプログラム

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/RegisterFamilyName.java
@@ -0,0 +1,15 @@
+//山﨑里沙,2022/12/27
+//家族のグループ名を処理するプログラム
+package com.example.foodmanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterFamilyName extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_familyname);
+    }
+}
