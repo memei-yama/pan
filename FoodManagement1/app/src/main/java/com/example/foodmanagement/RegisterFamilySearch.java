85f6819c7d88eb2eda222f302dae0b246a5631ad
yamasaki-risa <risakinoko@icloud.com>
2022-12-27 04:38:58 +0000

    家族ユーザ検索を行うプログラム

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/RegisterFamilySearch.java
@@ -0,0 +1,15 @@
+//山﨑里沙,2022/12/27
+//家族のユーザ検索をするプログラム
+package com.example.foodmanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterFamilySearch extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_family);
+    }
+}
