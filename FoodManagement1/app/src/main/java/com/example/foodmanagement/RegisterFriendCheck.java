8aa27c6f53edc832df4d0892a30cdc4c30515f85
yamasaki-risa <risakinoko@icloud.com>
2022-12-29 14:36:18 +0000

    フレンド登録の実装

---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/RegisterFriendCheck.java
@@ -0,0 +1,15 @@
+//山﨑里沙,2022/12/29
+//フレンドユーザを検索結果を確認するプログラム
+package com.example.foodmanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterFriendCheck extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_friendcheck);
+    }
+}
---
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/RegisterFriendFin.java
@@ -0,0 +1,15 @@
+//山﨑里沙,2022/12/29
+//フレンド完了確認プログラム
+package com.example.foodmanagement;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterFriendFin extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_friendfin);
+    }
+}
