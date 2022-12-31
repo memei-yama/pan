0bd028ed44392b5632452a7a80261ed9777aad3d
shiba-mn <kekhi03918@yahoo.co.jp>
2023-01-01 00:34:39 +0900

    ログイン画面

---
+++ Master_PHP/AdminLogin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>sample</title>
+    </head>
+    <body>
+        <form action="confirmation.php" method="post">
+            ユーザ名：<br />
+            <input type="text" name="name" size="50" value="" /><br />
+            パスワード：<br />
+            <input type="text" name="passwd" size="50" value="" /><br />
+            <br />
+ 
+            <input type="submit" value="ログイン" />
+        </form>
+    </body>
+</html>
