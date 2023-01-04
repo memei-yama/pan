5ebe5d0921e9b2beefa430f4844066fe4ad1600b
memei-yama <mei01yam12@yahoo.co.jp>
2023-01-04 23:32:39 +0900

    画面遷移追加

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/FoodDetails.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/FoodDetails.java
@@ -4,13 +4,44 @@
 package com.example.foodmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class FoodDetails extends AppCompatActivity {
+public class FoodDetails extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mypage);
+        setContentView(R.layout.food_details);
+
+        //各種ボタン設定
+        //戻る
+        findViewById(R.id.button36).setOnClickListener(this);
+        //更新
+        findViewById(R.id.button35).setOnClickListener(this);
+        //削除
+        findViewById(R.id.button33).setOnClickListener(this);
+
+        //お気に入り
+        //findViewById(R.id.button33).setOnClickListener(this);
+    }
+
+    //画面遷移
+    @Override
+    public void onClick(View v){
+        Intent home = new Intent(this, Home.class);
+        Intent food_update = new Intent(this, FoodUpdate.class);
+        Intent food_delete = new Intent(this, FoodDelete.class);
+
+        if (v.getId() == R.id.button36){
+            //ホーム画面
+            startActivity(home);
+        }else if (v.getId() == R.id.button35){
+            //食品更新
+            startActivity(food_update);
+        }else if (v.getId() == R.id.button33){
+            startActivity(food_delete);
+        }
+
     }
 }
