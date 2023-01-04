45f845f643f249296f7122dc4f4210b3723a92f4
memei-yama <mei01yam12@yahoo.co.jp>
2023-01-04 20:05:17 +0900

    各種ボタンの処理追加

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/RegisterFood.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/RegisterFood.java
@@ -3,17 +3,32 @@
 
 package com.example.foodmanagement;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
 import android.view.View;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.EditText;
 
+//calender
+import android.app.DatePickerDialog;
+import android.widget.DatePicker;
+import android.icu.util.Calendar;
+
+//個数選択
+import android.widget.NumberPicker;
+
+
 public class RegisterFood extends AppCompatActivity implements View.OnClickListener{
 
     //変数(テキスト)の設定
     private EditText food_name;
+    private EditText food_date;
     private EditText food_shop;
+    private EditText food_num;
+    //private NumberPicker food_num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,24 +42,61 @@ public class RegisterFood extends AppCompatActivity implements View.OnClickListe
 
 
         //食品名　（テキスト）
-        //food_name = findViewById(R.id.)
+        food_name = (EditText) findViewById(R.id.foodName);
 
         //賞味・消費選択
 
         //期限　（カレンダー）
+        food_date = (EditText) findViewById(R.id.editTextDate);
+
+        food_date.setOnClickListener(new View.OnClickListener() {
+                     @RequiresApi(api = Build.VERSION_CODES.N)
+                     @Override
+                     public void onClick(View v) {
+                         final Calendar date = Calendar.getInstance();
+
+                         DatePickerDialog datePickerDialog = new DatePickerDialog(
+                                 RegisterFood.this,
+                                 new DatePickerDialog.OnDateSetListener() {
+                                     @Override
+                                     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                         //setした日付を取得して表示
+                                         food_date.setText(String.format("%d / %02d / %02d", year, month + 1, dayOfMonth));
+                                     }
+                                 },
+                                 date.get(Calendar.YEAR),
+                                 date.get(Calendar.MONTH),
+                                 date.get(Calendar.DATE)
+                         );
+                         //dialogを表示
+                         datePickerDialog.show();
+
+                     }
+        });
+
 
         //購入店舗（テキスト）
-        //food_shop = findViewById(R.id.)
+        food_shop = (EditText) findViewById(R.id.shopName);
 
         //個数　（個数選択）
+        food_num = (EditText) findViewById(R.id.foodNum);
+
+        //food_num = (NumberPicker) findViewById(R.id.foodNum);
+
+        //上限、下限値、初期値設定
+        //food_num.setMaxValue(50);
+        //food_num.setMinValue(1);
+        //food_num.setValue(1);
 
 
-        //追加ボタン
-        //findViewById(R.id.button ).setOnClickListener(this);
-
-        //削除ボタン
-        //findViewById(R.id.button ).setOnClickListener(this);
+        //戻るボタン
+        findViewById(R.id.button37).setOnClickListener(this);
+        //登録ボタン
+        findViewById(R.id.button38).setOnClickListener(this);
     }
+
+
+
 
 
     //画面遷移
@@ -57,20 +109,37 @@ public class RegisterFood extends AppCompatActivity implements View.OnClickListe
 
         //カメラ読み取りボタンを押した時
         if (v.getId() == R.id.button ){
+            //カメラ読み取り
+            registerFoodJAN();
 
         }else if (v.getId() == R.id.button ){ //手入力ボタンを押した時
 
 
+            String get_food_name = food_name.getText().toString();
+            //String get_food_name = food_name.getText().toString();
+            //String get_food_name = food_name.getText().toString();
+            String get_food_shop = food_shop.getText().toString();
+
+
             //food_nameとfood_dateの確認
-
-
-            startActivity(home);
+            if(!get_food_name.equals("") ){
+                //データベースに登録する処理
+                startActivity(home);
+            }else{
+                //エラー画面に
+                startActivity(home);
+            }
 
 
         }
 
 
 
+
+
+    }
+
+    public static void registerFoodJAN(){
 
 
     }
