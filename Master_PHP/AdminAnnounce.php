d1ba4e3da9319f571496005c20a9ca59ee17d612
shiba-mn <kekhi03918@yahoo.co.jp>
2023-01-05 10:31:12 +0900

    タイトルの変更

--- Master_PHP/AdminAnnounce.php
+++ Master_PHP/AdminAnnounce.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>sample</title>
+        <title>お知らせ</title>
     </head>
     <body>
         <form action="confirmation.php" method="post">
