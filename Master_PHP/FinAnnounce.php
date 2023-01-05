0cab24927243908b7d9460bb79541f4966930aee
shiba-mn <kekhi03918@yahoo.co.jp>
2023-01-05 10:29:23 +0900

    送信完了画面画面

---
+++ Master_PHP/FinAnnounce.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>完了</title>
+</head>
+ 
+<body>
+ 
+<h2>お知らせ送信完了</h2>    
+ 
+<input type="submit" value="戻る" />
+
+</body>
+    
+</html>
