a271a6db00b7fbac00dea5ec3715682d1c3007ad
yamasaki-risa <risakinoko@icloud.com>
2023-01-06 00:48:20 +0000

    新規登録のエラー画面を追加

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/SignUpError.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/SignUpError.java
@@ -1,5 +1,5 @@
 //山﨑里沙,2022/12/22
-//ログインの際の必要事項が入力されてないときに画面を表示するプログラム
+//新規登録の際の必要事項が入力されてないときに画面を表示するプログラム
 package com.example.foodmanagement;
 
 import android.content.Intent;
@@ -8,20 +8,20 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginError extends AppCompatActivity implements View.OnClickListener{
+public class SignUpError extends AppCompatActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_error);
+        setContentView(R.layout.signup_error);
 
-        findViewById(R.id.button39).setOnClickListener(this);//戻るボタン
+        findViewById(R.id.button40).setOnClickListener(this);//戻るボタン
     }
     //ボタンを押下したらログインに遷移する
     @Override
     public void onClick(View v) {
-        Intent fin = new Intent(this, LoginAccount.class);
+        Intent fin = new Intent(this, SignUp.class);
 
-        if (v.getId() == R.id.button39) {
+        if (v.getId() == R.id.button40) {
             startActivity(fin);
         }
     }
