baf1137329254ac3988f6b0b5720857d70421230
KatsutaToko <u7hxfxzhz@i.softbank.jp>
2023-01-12 13:51:51 +0900

    送信先ユーザ検索を削除

--- PHP/chatroom.php
+++ PHP/chatroom.php
@@ -10,6 +10,7 @@
 //チャット内容取得
 function getChat($room_id){
     list($serial_number, $date, $char_body, $user_id) = chatroomDB.getChat($room_id);
+    notificationDB.updateCheckTime();
     return array($serial_number, $date, $char_body, $user_id);
 }
 
@@ -19,11 +20,12 @@ function addChat($room_id, $user_id, $char_body){
     return array($serial_number, $date);
 }
 
-//送信先ユーザ検索
+/*送信先ユーザ検索
 function searchSendUser($room_id, $user_id){
     list($send_user) = chatroomDB.searchSendUser($room_id, $user_id);
     return $send_user;
 }
+*/
 
 
 ?>
