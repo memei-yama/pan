ced7a07c74f23309e621182b6a471ce9c83f92ca
KatsutaToko <u7hxfxzhz@i.softbank.jp>
2023-01-12 14:09:01 +0900

    承認請求IDに伴う記述の変更

--- PHP/familyProcess.php
+++ PHP/familyProcess.php
@@ -15,7 +15,7 @@ function makeFamilyGroup($family_name){
 
 //ユーザ検索
 function searchUser($user_mail){
-    list($user_id, $user_name) = familyDB.searchUser();
+    list($user_id, $user_name) = familyDB.searchUser($user_mail);
     return array($user_id, $user_name);
 }
 
@@ -24,14 +24,30 @@ function updateFamily($family_id, $family_name){
     familyDB.updateFamily($family_id, $family_name);
 }
 
+
+//家族登録請求（ユーザを家族に追加）
+function familyAddRequest($family_id, $user_id){
+    $request_id = familyProcess . familyAddRequest($family_id, $user_id);
+    return $request_id;
+}
 //家族登録（ユーザを家族に追加）
-function familyAdd($family_id, $user_id){
-    $group_id = familyDB.familyAdd($family_id, $family_name);
+function familyAdd($request_id){
+    list($family_id, $user_id) = familyDB . getFamilyAddRequest($request_id);
+    $group_id = familyDB.familyAdd($family_id, $user_id);
+    familyDB . deleteFamilyAddRequest($request_id);
     return $group_id;
 }
+//フレンド登録請求
+function friendAddRequest($friend_id1, $friend_id2){
+    $request_id = familyDB.friendAddRequest($friend_id1, $friend_id2);
+    return $request_id;
+}
+
 //フレンド登録
-function friendAdd($friend_id1, $friend_id2){
-    $group_id = familyDB.friendAdd($friend_id1, $friend_id2);
+function friendAdd($request_id){
+    list(friend_id1, friend_id2) = familyDB . getFriendAddRequest($request_id);
+    $group_id = familyDB . friendAdd(Friend_id1, friend_id2);
+    familyDB . deleteFriendAddRequest($request_id);
     return $group_id;
 }
 
@@ -48,8 +64,8 @@ function searchFamily($user_id){
 
 //フレンド情報取得
 function searchFriend($user_id){
-    list($group_id, $friend_user) = familyDB.searchFriend($user_id);
-    return array($group_id, $friend_user);
+    list($group_id, $frienduser_id, $frienduser_name) = familyDB.searchFriend($user_id);
+    return array($group_id, $frienduser_id, $frienduser_name);
 }
 
 
