0e7cc77219d1a4a64062456191732c9cc9cd706d
240301e <240301e@ugs.kochi-tech.ac.jp>
2023-01-23 14:05:51 +0900

    ListView表示中かつphp接続挑戦中

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/TimelineMain.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/TimelineMain.java
@@ -2,11 +2,22 @@
 //タイムライン画面を表示するプログラム
 package com.example.foodmanagement;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+/*
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+*/
 
 public class TimelineMain extends AppCompatActivity implements View.OnClickListener {
     @Override
@@ -15,6 +26,20 @@ public class TimelineMain extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.timeline_main);
 
         findViewById(R.id.button30).setOnClickListener(this); //新規投稿作成ボタン
+
+        //仮データ
+        String[] names = {"ゆみ", "はなこ", "たくや", "まさお"};
+
+
+
+        ArrayList<Map<String, Object>> listData = new ArrayList<>();
+        for (int i=0; i < names.length; i++) {
+            Map<String, Object> item = new HashMap<>();
+            item.put("displayUser1", names[i]);
+            listData.add(item);
+
+        //AsyncTaskClass task = new AsyncTaskClass();
+        //task.execute("");
     }
 
     @Override
@@ -23,5 +48,36 @@ public class TimelineMain extends AppCompatActivity implements View.OnClickListe
         startActivity(send);
     }
 
+
+
     //データベースからTimelineListを取得しtextviewに表示させる
+/*
+    public class AsyncTaskClass extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground (String... params) {
+            //バックグラウンドで行う処理
+            Request request = new Request.Builder()
+                    .url("http://appserver/getTimeline?timeline_id=1124")
+                    .build();
+
+            OkHttpClient client = new OkHttpClient();
+
+            try {
+                Response response = client.newCall(request).execute();
+
+                String strData = response.body().string();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return strData;
+        }
+
+        @Override
+        protected void onPostExecute(String str) {
+            //UIスレッドに反映する処理
+        }
+    }
+
+ */
 }
