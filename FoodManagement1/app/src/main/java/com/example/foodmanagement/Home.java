9142cce73c207d24a4f64ae53f4216afda649883
memei-yama <mei01yam12@yahoo.co.jp>
2023-01-25 23:04:33 +0900

    ベルトを追加

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/Home.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/Home.java
@@ -6,6 +6,7 @@ package com.example.foodmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
 import android.content.Intent;
 
@@ -25,6 +26,15 @@ public class Home extends AppCompatActivity implements View.OnClickListener{
         //削除モード
         //findViewById(R.id.delFood).setOnClickListener(this);
 
+        //ホームボタン
+        findViewById(R.id.homeButton).setOnClickListener(this);
+        //マイページ
+        findViewById(R.id.mypageButton).setOnClickListener(this);
+        //タイムライン
+        findViewById(R.id.timelineButton).setOnClickListener(this);
+        //メッセージ
+        findViewById(R.id.messageButton).setOnClickListener(this);
+
 
         //表示
         //mGridView = (GridView)findViewById(R.id.food_grid);
@@ -32,6 +42,7 @@ public class Home extends AppCompatActivity implements View.OnClickListener{
 
         //RegisterFoodからの値を取得
         Intent intent = getIntent();
+
         String food_name = intent.getStringExtra("get_food_name");
         String food_date = intent.getStringExtra("get_food_date");
         String spinnerItems = intent.getStringExtra("get_spinnerItems");
@@ -51,12 +62,29 @@ public class Home extends AppCompatActivity implements View.OnClickListener{
         Intent favorite = new Intent(this, Favorite.class);
         Intent delete_mode = new Intent(this, DeleteMode.class);
 
+        Intent home = new Intent(this, Home.class);
+        Intent mypage = new Intent(this, Account.class);
+        Intent timeline = new Intent(this, TimelineMain.class);
+        Intent message = new Intent(this, Message.class);
+
 
         //どのボタンが押下されたか、それによる画面遷移
         if (v.getId() == R.id.regFood){ //食品登録ボタン
             startActivity(food_register_menu);
         } else if (v.getId() == R.id.delFood){ //削除モードボタン
             startActivity(delete_mode);
+
+        }else if (v.getId() == R.id.homeButton) { //ホームボタン
+            startActivity(home);
+
+        }else if (v.getId() == R.id.mypageButton) { //マイページボタン
+            startActivity(mypage);
+
+        }else if (v.getId() == R.id.timelineButton) { //タイムラインボタン
+            startActivity(timeline);
+
+        }else if (v.getId() == R.id.messageButton) { //メッセージボタン
+            startActivity(message);
         }
 
     }
