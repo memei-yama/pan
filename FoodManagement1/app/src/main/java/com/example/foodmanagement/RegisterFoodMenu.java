5947a7fd5838029874275b529375f35674bd983e
memei-yama <mei01yam12@yahoo.co.jp>
2023-01-25 22:34:44 +0900

    ベルトのボタン処理を追加

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/RegisterFoodMenu.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/RegisterFoodMenu.java
@@ -5,6 +5,7 @@ package com.example.foodmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.View;
 import android.content.Intent;
 
@@ -19,6 +20,14 @@ public class RegisterFoodMenu extends AppCompatActivity implements View.OnClickL
         findViewById(R.id.button45).setOnClickListener(this);
         //手入力
         findViewById(R.id.button44).setOnClickListener(this);
+        //ホームボタン
+        findViewById(R.id.homeButton).setOnClickListener(this);
+        //マイページ
+        findViewById(R.id.mypageButton).setOnClickListener(this);
+        //タイムライン
+        findViewById(R.id.timelineButton).setOnClickListener(this);
+        //メッセージ
+        findViewById(R.id.messageButton).setOnClickListener(this);
     }
 
     //画面遷移
@@ -27,13 +36,29 @@ public class RegisterFoodMenu extends AppCompatActivity implements View.OnClickL
         //今はどちらもRegisterFoodに遷移するようにしている
         Intent register_camera = new Intent(this, RegisterFoodCamera.class);
         Intent register_self = new Intent(this, RegisterFood.class);
-
+        Intent home = new Intent(this, Home.class);
+        Intent mypage = new Intent(this, Account.class);
+        Intent timeline = new Intent(this, TimelineMain.class);
+        Intent message = new Intent(this, Message.class);
 
         //どのボタンが押下されたか、それによる画面遷移
         if (v.getId() == R.id.button45){ //カメラ読み取りボタン
             startActivity(register_camera);
+
         } else if (v.getId() == R.id.button44){ //手入力ボタン
             startActivity(register_self);
+
+        }else if (v.getId() == R.id.homeButton) { //ホームボタン
+            startActivity(home);
+
+        }else if (v.getId() == R.id.mypageButton) { //マイページボタン
+            startActivity(mypage);
+
+        }else if (v.getId() == R.id.timelineButton) { //タイムラインボタン
+            startActivity(timeline);
+
+        }else if (v.getId() == R.id.messageButton) { //メッセージボタン
+            startActivity(message);
         }
 
     }
