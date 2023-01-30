482ed44527a72b66d0592f76f42626c524580a92
240301e <240301e@ugs.kochi-tech.ac.jp>
2023-01-31 08:36:36 +0900

    戻るボタンとベルトのボタンによる画面遷移を追加

--- FoodManagement1/app/src/main/java/com/example/foodmanagement/TimelineReaction.java
+++ FoodManagement1/app/src/main/java/com/example/foodmanagement/TimelineReaction.java
@@ -4,7 +4,9 @@ package com.example.foodmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -12,7 +14,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TimelineReaction extends AppCompatActivity{
+public class TimelineReaction extends AppCompatActivity implements View.OnClickListener{
+    int button = 3;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,5 +39,40 @@ public class TimelineReaction extends AppCompatActivity{
                 new String[] {"displayUser1"},
                 new int[] {R.id.displayUser1}
         ));
+
+        findViewById(R.id.button50).setOnClickListener(this);
+
+        findViewById(R.id.homeButton).setOnClickListener(this);//ホーム
+        findViewById(R.id.mypageButton).setOnClickListener(this);//マイページ
+        findViewById(R.id.timelineButton).setOnClickListener(this);//タイムライン
+        findViewById(R.id.messageButton).setOnClickListener(this);//メッセージ
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent back = new Intent(this, TimelineMain.class);
+        startActivity(back);
+
+        //ベルトのボタン処理
+        Intent home = new Intent(this, Home.class);
+        Intent mypage = new Intent(this, Account.class);
+        Intent timeline = new Intent(this, TimelineMain.class);
+        Intent talk = new Intent(this, TalkMain.class);
+        if (v.getId() == R.id.homeButton && button != 1) {
+            startActivity(home);
+            button = 1;
+
+        } else if (v.getId() == R.id.mypageButton && button != 2) {
+            startActivity(mypage);
+            button = 2;
+
+        } else if (v.getId() == R.id.timelineButton && button != 3) {
+            startActivity(timeline);
+            button = 3;
+
+        } else if (v.getId() == R.id.messageButton && button != 4) {
+            startActivity(talk);
+            button = 4;
+        }
     }
 }
